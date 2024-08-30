6c9a1dc821314a5eb4a260916901628556c4c424
sultanarabi161 <168210639+sultanarabi161@users.noreply.github.com>
2024-08-30 21:16:38 +0600

    main.js

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-const navLinks = document.querySelectorAll(".nav-link");
+const player = new Plyr('#videoPlayer', {
+            controls: [
+                'play-large', 'restart', 'rewind', 'play', 'fast-forward', 
+                'progress', 'current-time', 'duration', 'mute', 
+                'volume', 'captions', 'settings', 'pip', 'airplay', 
+                 'fullscreen'
+            ]
+        });
 
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    removeActiveClass();
-    navLink.classList.add("active");
-    window.location.href = navLink.href;
-  });
-});
+        document.querySelectorAll('.channel').forEach(function(channel) {
+            channel.addEventListener('click', function() {
+                var videoSrc = this.getAttribute('data-video-src');
+                player.source = {
+                    type: 'video',
+                    sources: [
+                        {
+                            src: videoSrc,
+                            type: 'application/x-mpegURL'
+                        }
+                    ]
+                };
+                player.play();
+            });
+        });
 
-function removeActiveClass() {
-  navLinks.forEach((navLink) => {
-    navLink.classList.remove("active");
-  });
-}
-
-var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-  },
-});
+        document.querySelectorAll('.category').forEach(function(category) {
+            category.addEventListener('click', function() {
+                var selectedCategory = this.getAttribute('data-category');
+                document.querySelectorAll('.channel').forEach(function(channel) {
+                    if (channel.getAttribute('data-category') === selectedCategory) {
+                        channel.style.display = 'block';
+                    } else {
+                        channel.style.display = 'none';
+                    }
+                });
+            });
+        });
